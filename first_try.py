036244227036290ac4e2192b42e064c4cd9e5819
Joe Letizia <joeletizia@agar.(none)>
2013-11-24 16:04:10 -0500

    first attempt finished

--- first_try.py
+++ first_try.py
@@ -2,32 +2,78 @@ import math
 import rg
 
 class Robot:
+    game = None
     def act(self, game):
-        return self.move_to_center()
-    def move_up(self):
-        return ['move', (self.location[0],self.location[1]-1)]
-    def move_down(self):
-        return ['move', (self.location[0],self.location[1]+1)]
-    def move_left(self):
-        return ['move', (self.location[0]-1,self.location[1])]
-    def move_right(self):
-        return ['move', (self.location[0]+1,self.location[1])]
+        self.game = game
+        if self.got_backup() or self.location == rg.CENTER_POINT:
+            enemy_location = self.closest_adjacent_enemy()
+            if enemy_location:
+                # print("Robot {0}:Trying to attack. Location:{1}".format(str(self.location), str(enemy_location)))
+                return self.attack(enemy_location)
+            else:
+                # print("Robot {0}:No enemies around; guarding.".format(self.location))
+                return self.guard()
+        else:
+            # print("Robot {0}: Not in position; moving to {1}.".format(self.location, self.where_i_should_move()))
+            return self.move(self.where_i_should_move())
+    def closest_adjacent_enemy(self):
+        if self.enemy_in_space(self.right()):
+            return self.right()
+        if self.enemy_in_space(self.left()):
+            return self.left()
+        if self.enemy_in_space(self.up()):
+            return self.up()
+        if self.enemy_in_space(self.down()):
+            return self.down()
+    def right(self):
+        return (self.location[0]+1, self.location[1])
+    def left(self):
+        return (self.location[0]-1, self.location[1])
+    def up(self):
+        return (self.location[0], self.location[1]-1)
+    def down(self):
+        return (self.location[0], self.location[1]+1)
     def guard(self):
         return ['guard']
-    def move_to_center(self):
+    def attack(self, location):
+        return ['attack', location]
+    def move(self, location):
+        return ['move', location]
+    def where_i_should_move(self):
         if self.location == rg.CENTER_POINT:
-            return self.guard()
+            return rg.CENTER_POINT
         dist_tuple = self.distance_to_center()
         if math.fabs(dist_tuple[0]) >= math.fabs(dist_tuple[1]):
             if dist_tuple[0] > 0:
-                return self.move_right()
+                return self.right()
             else:
-                return self.move_left()
+                return self.left()
         else:
             if dist_tuple[1] > 0:
-                return self.move_down()
+                return self.down()
             else:
-                return self.move_up()
+                return self.up()
     def distance_to_center(self):
         return (rg.CENTER_POINT[0] - self.location[0],
                 rg.CENTER_POINT[1] - self.location[1])
+    def got_backup(self):
+        next_location = self.where_i_should_move()
+        if self.enemy_in_space(next_location):
+            return False
+        if self.ally_in_space(next_location):
+            return True
+    def space_occupied(self, location):
+        if self.game.robots.get(location, False):
+            return True
+        else:
+            False
+    def enemy_in_space(self, location):
+        if self.space_occupied(location):
+            return self.game.robots[location].player_id != self.player_id
+        else: 
+            False
+    def ally_in_space(self, location):
+        if self.space_occupied(location):
+            return self.game.robots[location].player_id == self.player_id
+        else:
+            False
